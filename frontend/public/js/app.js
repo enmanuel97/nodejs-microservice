6fec04dcd2a08f595d92184f0a2aa64e57847a2d
Jesus Enmanuel <edelacruz9713@gmail.com>
2021-12-17 10:54:41 -0400

    cambios

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -1,10 +1,93 @@
 import axios from 'axios';
 import Swal from 'sweetalert2';
 
-if(document.querySelector('.aceptar-orden') !== null) {
-	document.querySelector('.aceptar-orden').addEventListener('click', function(e) {
-		
-	});
+if(document.querySelectorAll('.start-preparation') !== null) {
+	var buttonStart = document.querySelectorAll('.start-preparation');
+	for(var i = 0; i < buttonStart.length; i++) {
+		buttonStart[i].addEventListener('click', function(e) {
+			var button = e.target;
+			const url = `${location.origin}/cocina/preparar/${button.getAttribute('data-id')}`;
+			axios.get(url).then(function(response) {
+				if(response.data.status === 'success') {
+					Swal.fire({
+						title: '¡Listo!',
+						text: 'La orden se ha preparado correctamente',
+						type: 'success'
+					});
+					button.parentElement.parentElement.remove();
+				} else if(response.data.status == 'unavailable' && response.data.without_stock == true) {
+					Swal.fire({
+						title: 'Sin Stock',
+						text: 'No hay stock suficiente para preparar la orden, ¿Deseas realizar una compra?',
+						type: 'error',
+						showCancelButton: true,
+						confirmButtonColor: '#3085d6',
+						cancelButtonColor: '#d33',
+						confirmButtonText: 'Si, comprar',
+						cancelButtonText : 'No, Cancelar'
+					}).then((result) => {
+						if (result.value) {
+							Swal.fire({
+								title: 'Ingrese la cantidad que deseas comprar',
+								input: 'number',
+								inputAttributes: {
+								  	min: 1
+								},
+								showCancelButton: true,
+								confirmButtonText: 'Comprar',
+								showLoaderOnConfirm: true,
+								preConfirm: (cantidad) => {
+									const url = `${location.origin}/plaza/comprar/${cantidad}`;
+									console.log(url)
+								},
+								allowOutsideClick: () => !Swal.isLoading()
+							}).then((result) => {
+								if (result.isConfirmed) {
+									Swal.fire({
+										title: '¡Listo!',
+										text: 'La compra se realizdo correctamente, ¿Deseas continuar con la preparacion?',
+										type: 'info',
+										showCancelButton: true,
+										confirmButtonColor: '#3085d6',
+										cancelButtonColor: '#d33',
+										confirmButtonText: 'Si, preparar',
+										cancelButtonText : 'No, Cancelar'
+									}).then((result) => {
+										if (result.value) {
+											const url = `${location.origin}/cocina/preparar/${button.getAttribute('data-id')}`;
+											axios.get(url).then(function(response) {
+												if(response.data.status === 'success') {
+													Swal.fire({
+														title: '¡Listo!',
+														text: 'La orden se ha preparado correctamente',
+														type: 'success'
+													});
+													button.parentElement.parentElement.remove();
+												} else {
+													Swal.fire({
+														title: 'Error',
+														text: 'Hubo un error al preparar la orden intente mas tarde',
+														type: 'error'
+													});
+												}
+											});
+										}
+									});
+								}
+							})
+						}
+					})
+				} else {
+					Swal.fire({
+						title: '¡Error!',
+						text: 'Ha ocurrido un error al preparar la orden',
+						type: 'error'
+					});
+				}
+			});
+			console.log(url);
+		});
+	}
 }
 
 if(document.querySelectorAll('.ingredient') !== null) {
